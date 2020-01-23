4d57d5098941e9a7db5051522a8a991404c6ec58
Elenandreeva <confeta@bk.ru>
2020-01-23 15:40:11 +0300

    +

---
+++ lesson1/js/main.js
@@ -0,0 +1,12 @@
+let money = 0, 
+        income = 1,
+        addExpenses = 0,
+        deposit = 0,
+        mission = 0,
+        period = 0;
+
+alert('Любое сообщение');
+console.log("Here is the message");
+
+
+console.log(income);
